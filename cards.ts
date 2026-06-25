0ff2d453946a5634ecf73db4db13bf4ee7886076
Alexandre Sieg <sieg.alexandre@gmail.com>
2026-06-25 13:28:03 +0200

    feat: add card logic

---
+++ cards.ts
@@ -0,0 +1,42 @@
+type Card = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | 'STOP' | '3_IN_A_ROW' | 'SECOND_CHANCE' | '+2' | '+4' | '+6' | '+8' | '+10' | 'x2';
+
+export default class Cards {
+  public cards: Card[] = [];
+  public bin: Card[] = [];
+
+  constructor() {}
+
+  initializeDeck() {
+    this.cards = ['0', '+2', '+4', '+6', '+8', '+10', 'x2'];
+    for (let i = 0; i <= 12; i++) {
+      for (let j = 0; j < i; j++) {
+        this.cards.push(i.toString() as Card);
+      }
+    }
+    // Add 3 sample of each special card
+    for (let i = 0; i < 3; i++) {
+      this.cards.push('STOP', '3_IN_A_ROW', 'SECOND_CHANCE');
+    }
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  drawCard() {
+    if (this.cards.length === 0) {
+      console.log('Reshuffling deck... (No card left)');
+      this.cards = [...this.bin];
+      this.bin = [];
+      this.shuffle();
+    }
+    return this.cards.pop();
+  }
+
+  addCardToBin(card: Card) {
+    this.bin.push(card);
+  }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
+import Cards from "./cards.ts"
+
 console.log('Flip 7')
+
+const deck = new Cards()
+deck.initializeDeck()
+deck.shuffle()
+console.log(deck.cards)
