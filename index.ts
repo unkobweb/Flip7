2c3d450272c2e8906774966f64b029e259e9fa99
Alexandre Sieg <sieg.alexandre@gmail.com>
2026-06-25 15:44:32 +0200

    feat: can add several players

--- index.ts
+++ index.ts
@@ -1,11 +1,42 @@
 import Game from "./game.ts";
 import Player from "./player.ts";
+import c from 'chalk'
+import { select, input } from '@inquirer/prompts'
 
-console.log('Welcome to Flip 7!')
+const players = []
 
-const game = new Game([
-  new Player('Alexandre'),
-  new Player('John'),
-])
+function greetingMessage() {
+  console.log(c.bold.green('Welcome to Flip 7!'))
+}
+
+greetingMessage()
+const playerName = await input({ message: 'Enter first player name:' })
+players.push(new Player(playerName))
+
+console.clear()
+greetingMessage()
+const secondPlayerName = await input({ message: 'Enter second player name:' })
+players.push(new Player(secondPlayerName))
+
+let choice = 'play'
+do {
+  console.clear()
+  greetingMessage()
+  console.log('Players:', players.map(p => p.name).join(', '))
+  choice = await select({
+    message: 'What do you want to do?',
+    choices: [
+      { name: 'Play', value: 'play' },
+      { name: 'Add player', value: 'add' },
+    ],
+  })
+
+  if (choice === 'add') {
+    const playerName = await input({ message: 'Enter player name:' })
+    players.push(new Player(playerName))
+  }
+} while (choice !== 'play')
+
+const game = new Game(players)
 
 game.start()
